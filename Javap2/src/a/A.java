f04bb0a1b34b67988fefa9d4a9b8dfc908bb9b1d
Akash 1 <Akash@192.168.0.23>
2017-11-18 17:58:30 +0530

    commit hi hi1

---
+++ Javap2/src/a/A.java
@@ -0,0 +1,14 @@
+package a;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+System.out.println("hi 1");
+System.out.println("hi 1");
+
+
+	}
+
+}
