fbadd094679802a25e95f1cf5648cabd03a49528
Akash <Akash@192.168.0.23>
2017-11-18 18:10:14 +0530

    commi n

---
+++ Javappp/src/a/AA.java
@@ -0,0 +1,13 @@
+package a;
+
+public class AA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi 1");
+System.out.println("hi 1");
+System.out.println("hi 1");
+
+	}
+
+}
